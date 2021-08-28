860a71c44b30a3fe24607878ba0ebf29848bd0f7
jrrholanda <87144681+jrrholanda@users.noreply.github.com>
2021-08-28 18:51:45 -0300

    Não pode haver mais de uma proposta para um mesmo documento

--- src/main/java/br/com/zup/propostas/proposta/PropostaController.java
+++ src/main/java/br/com/zup/propostas/proposta/PropostaController.java
@@ -1,9 +1,11 @@
 package br.com.zup.propostas.proposta;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -22,6 +24,11 @@ public class PropostaController {
     public ResponseEntity<?> novaProposta(@Valid @RequestBody NovaPropostaRequest request, UriComponentsBuilder uriComponentsBuilder){
 
         Proposta proposta = request.toModel();
+
+        if(repository.existsByDocumento(proposta.getDocumento())){
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Proposta já existente");
+        }
+
         repository.save(proposta);
 
         URI link = uriComponentsBuilder.path("/api/propostas/{id}").buildAndExpand(proposta.getId()).toUri();
--- src/main/java/br/com/zup/propostas/proposta/PropostaRepository.java
+++ src/main/java/br/com/zup/propostas/proposta/PropostaRepository.java
@@ -3,4 +3,5 @@ package br.com.zup.propostas.proposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+    boolean existsByDocumento(String documento);
 }
